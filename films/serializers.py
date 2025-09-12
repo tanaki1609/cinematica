c8de1b579d071a6e0524ded04d2b9efd0d51522b
tanaki <tanaki.93@gmail.com>
2025-09-12 21:17:36 +0600

    lesson 4

--- films/serializers.py
+++ films/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Film, Director, Review
+from rest_framework.exceptions import ValidationError
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -32,3 +34,19 @@ class FilmListSerializer(serializers.ModelSerializer):
 
     def get_genres(self, film):
         return [i.name for i in film.genres.all()]
+
+
+class FilmValidateSerializer(serializers.Serializer):
+    title = serializers.CharField(required=True, min_length=1, max_length=255)
+    text = serializers.CharField(required=False)
+    rating_kp = serializers.FloatField(min_value=0, max_value=10)
+    is_active = serializers.BooleanField()
+    director_id = serializers.IntegerField(min_value=1)
+    genres = serializers.ListField(child=serializers.IntegerField(min_value=1))
+
+    def validate_director_id(self, director_id):  # 100
+        try:
+            Director.objects.get(id=director_id)
+        except Director.DoesNotExist:
+            raise ValidationError('Director does not exist!')
+        return director_id
--- films/views.py
+++ films/views.py
@@ -2,7 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Film
-from .serializers import FilmListSerializer, FilmDetailSerializer
+from .serializers import (FilmListSerializer,
+                          FilmDetailSerializer,
+                          FilmValidateSerializer)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])  # GET->retrieve, PUT->update, DELETE->destroy
@@ -16,12 +18,19 @@ def film_detail_api_view(request, id):
         data = FilmDetailSerializer(film, many=False).data
         return Response(data=data)
     elif request.method == 'PUT':
-        film.title = request.data.get('title')
-        film.text = request.data.get('text')
-        film.rating_kp = request.data.get('rating_kp')
-        film.is_active = request.data.get('is_active')
-        film.director_id = request.data.get('director_id')
-        film.genres.set(request.data.get('genres'))
+        serializer = FilmValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data=serializer.errors
+            )
+
+        film.title = serializer.validated_data.get('title')
+        film.text = serializer.validated_data.get('text')
+        film.rating_kp = serializer.validated_data.get('rating_kp')
+        film.is_active = serializer.validated_data.get('is_active')
+        film.director_id = serializer.validated_data.get('director_id')
+        film.genres.set(serializer.validated_data.get('genres'))
         film.save()
         return Response(status=status.HTTP_201_CREATED,
                         data=FilmDetailSerializer(film).data)
@@ -45,13 +54,21 @@ def film_list_create_api_view(request):
             status=status.HTTP_200_OK  # status = int
         )
     elif request.method == 'POST':
-        # step 1: Receive data from RequestBody
-        title = request.data.get('title')
-        text = request.data.get('text')
-        rating_kp = request.data.get('rating_kp')
-        is_active = request.data.get('is_active')
-        director_id = request.data.get('director_id')
-        genres = request.data.get('genres')
+        # step 0: Validation (Existing, Typing, Extra)
+        serializer = FilmValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data=serializer.errors
+            )
+
+        # step 1: Receive data from ValidatedData
+        title = serializer.validated_data.get('title')
+        text = serializer.validated_data.get('text')
+        rating_kp = serializer.validated_data.get('rating_kp')
+        is_active = serializer.validated_data.get('is_active')
+        director_id = serializer.validated_data.get('director_id')
+        genres = serializer.validated_data.get('genres')
 
         # step 2: Create film by received data
         film = Film.objects.create(
