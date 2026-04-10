374f6a70d2dec56fcfd44635a606dbe5680452cb
tanaki <tanaki.93@gmail.com>
2026-04-10 18:59:32 +0600

    lesson 4

--- cinematica/settings.py
+++ cinematica/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'films',
     'users',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- users/views.py
+++ users/views.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterValidateSerializer
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+
+@api_view(['POST'])
+def authorization_api_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        try:
+            token = Token.objects.get(user=user)
+        except:
+            token = Token.objects.create(user=user)
+        return Response(data={'key': token.key})
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(['POST'])
@@ -16,7 +33,8 @@ def registration_api_view(request):
     password = request.data.get('password')
 
     # step 3: create user
-    user = User.objects.create_user(username=username, password=password)
+    user = User.objects.create_user(username=username, password=password,
+                                    is_active=False)
 
     # step 4: return response
     return Response(status=status.HTTP_201_CREATED,
