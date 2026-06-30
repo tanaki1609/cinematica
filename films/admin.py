3059561b6e149b9a924496c016d4311ae4f0d45c
tanaki <tanaki.93@gmail.com>
2026-06-30 19:09:55 +0600

    lesson 2

--- films/admin.py
+++ films/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Film, Director, Genre
+from .models import Film, Director, Genre, Review
 
 
 admin.site.register(Film)
 admin.site.register(Director)
 admin.site.register(Genre)
+admin.site.register(Review)
--- films/serializers.py
+++ films/serializers.py
@@ -22,13 +22,12 @@ class FilmDetailSerializer(serializers.ModelSerializer):
 
 class FilmListSerializer(serializers.ModelSerializer):
     director = DirectorSerializer(many=False)
-    # genres = GenreSerializer(many=True)
     genres = serializers.SerializerMethodField()
 
     class Meta:
         model = Film
-        fields = 'id title rating created director genres'.split()
-        # depth = 1
+        fields = 'id title rating created director genres reviews'.split()
+        depth = 1
 
     def get_genres(self, film):
         return film.genre_names()[0:2]
