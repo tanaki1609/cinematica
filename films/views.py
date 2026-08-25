adc95ee5633ba4e16079e79270876596f77155bd
tanaki <tanaki.93@gmail.com>
2026-08-25 18:46:32 +0600

    lesson 3

--- films/views.py
+++ films/views.py
@@ -5,27 +5,68 @@ from .models import Film
 from .serializers import FilmListSerializer, FilmDetailSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def film_detail_api_view(request, id):
     try:
         film = Film.objects.get(id=id)
     except Film.DoesNotExist:
         return Response(data={'error': 'Film not found!'},
                         status=status.HTTP_404_NOT_FOUND)
-    data = FilmDetailSerializer(film, many=False).data
-    return Response(data=data)
+    if request.method == 'GET':
+        data = FilmDetailSerializer(film, many=False).data
+        return Response(data=data)
+    elif request.method == 'DELETE':
+        film.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        film.title = request.data.get('title')
+        film.text = request.data.get('text')
+        film.release_year = request.data.get('release_year')
+        film.rating = request.data.get('rating')
+        film.is_hit = request.data.get('is_hit')
+        film.director_id = request.data.get('director_id')
+        film.genres.set(request.data.get('genres'))
+        film.save()
+        return Response(status=status.HTTP_201_CREATED,
+                        data=FilmDetailSerializer(film).data)
 
 
-@api_view(['GET'])
-def film_list_api_view(request):
-    # step 1: Collect films (QuerySet)
-    films = Film.objects.select_related('director').prefetch_related('genres', 'reviews').all()
+@api_view(['GET', 'POST'])
+def film_list_create_api_view(request):
+    if request.method == 'GET':
+        # step 1: Collect films (QuerySet)
+        films = Film.objects.select_related('director').prefetch_related('genres', 'reviews').all()
 
-    # step 2: Reformat QuerySet to list dictionary (Serializer)
-    list_ = FilmListSerializer(films, many=True).data
+        # step 2: Reformat QuerySet to list dictionary (Serializer)
+        list_ = FilmListSerializer(films, many=True).data
 
-    # step 3: Return response
-    return Response(
-        status=status.HTTP_200_OK,  # default (200) -> int
-        data=list_  # str, int, bool, dict, list
-    )
+        # step 3: Return response
+        return Response(
+            status=status.HTTP_200_OK,  # default (200) -> int
+            data=list_  # str, int, bool, dict, list
+        )
+    elif request.method == 'POST':
+        # step 1: Receive data from request body
+        title = request.data.get('title')
+        text = request.data.get('text')
+        rating = request.data.get('rating')
+        release_year = request.data.get('release_year')
+        is_hit = request.data.get('is_hit')
+        director_id = request.data.get('director_id')
+        genres = request.data.get('genres')
+
+        # step 2: Create film
+        film = Film.objects.create(
+            title=title,
+            text=text,
+            rating=rating,
+            release_year=release_year,
+            is_hit=is_hit,
+            director_id=director_id,
+        )
+        film.genres.set(genres)
+        film.save()
+
+        # step 3: Return response (status=201, data)
+        return Response(data=FilmDetailSerializer(film).data,
+                        status=status.HTTP_201_CREATED)
