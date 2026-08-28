21ff2a5ff24a24cb3619e50daa540e37a2ad2529
tanaki <tanaki.93@gmail.com>
2026-08-28 18:47:58 +0600

    lesson 4

--- films/serializers.py
+++ films/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Film, Director
+from .models import Film, Director, Genre
+from rest_framework.exceptions import ValidationError
 
 
 class FilmDetailSerializer(serializers.ModelSerializer):
@@ -25,3 +26,27 @@ class FilmListSerializer(serializers.ModelSerializer):
 
     def get_genres(self, film):
         return sorted(film.genre_names())
+
+
+class FilmValidateSerializer(serializers.Serializer):
+    title = serializers.CharField(required=True, max_length=255, min_length=1)
+    text = serializers.CharField(required=False)
+    release_year = serializers.IntegerField()
+    rating = serializers.FloatField(min_value=1, max_value=10)
+    is_hit = serializers.BooleanField(default=True)
+    director_id = serializers.IntegerField()
+    genres = serializers.ListField(child=serializers.IntegerField())
+
+    def validate_director_id(self, director_id):
+        try:
+            Director.objects.get(id=director_id)
+        except Director.DoesNotExist:
+            raise ValidationError('Director not found!')
+        return director_id
+
+    def validate_genres(self, genres):  # [1,2,10]
+        genres = list(set(genres))
+        genres_from_db = Genre.objects.filter(id__in=genres)  # [1,2]
+        if len(genres_from_db) != len(genres):
+            raise ValidationError('Genre does not exist!')
+        return genres
--- films/views.py
+++ films/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Film
-from .serializers import FilmListSerializer, FilmDetailSerializer
+from .serializers import FilmListSerializer, FilmDetailSerializer, FilmValidateSerializer
+from django.db import transaction
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -46,26 +47,33 @@ def film_list_create_api_view(request):
             data=list_  # str, int, bool, dict, list
         )
     elif request.method == 'POST':
-        # step 1: Receive data from request body
-        title = request.data.get('title')
-        text = request.data.get('text')
-        rating = request.data.get('rating')
-        release_year = request.data.get('release_year')
-        is_hit = request.data.get('is_hit')
-        director_id = request.data.get('director_id')
-        genres = request.data.get('genres')
+        # step 0: Validation (Existing, Typing, Extra)
+        serializer = FilmValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data=serializer.errors)
+
+        # step 1: Receive data from validated data
+        title = serializer.validated_data.get('title')
+        text = serializer.validated_data.get('text')
+        rating = serializer.validated_data.get('rating')
+        release_year = serializer.validated_data.get('release_year')
+        is_hit = serializer.validated_data.get('is_hit')  # "y"
+        director_id = serializer.validated_data.get('director_id')
+        genres = serializer.validated_data.get('genres')
 
         # step 2: Create film
-        film = Film.objects.create(
-            title=title,
-            text=text,
-            rating=rating,
-            release_year=release_year,
-            is_hit=is_hit,
-            director_id=director_id,
-        )
-        film.genres.set(genres)
-        film.save()
+        with transaction.atomic():
+            film = Film.objects.create(
+                title=title,
+                text=text,
+                rating=rating,
+                release_year=release_year,
+                is_hit=is_hit,
+                director_id=director_id,
+            )
+            film.genres.set(genres)
+            film.save()
 
         # step 3: Return response (status=201, data)
         return Response(data=FilmDetailSerializer(film).data,
